1208b8619fcf4c889bde110962ad5e4f856546eb
Jeta Adhisatya <115772528+JetaAdhisatya@users.noreply.github.com>
2022-11-09 17:16:11 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,33 +17,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/home',function(){
-    $blog_posts = [
-        [
-            "judul" => "My Superman",
-            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolorum dignissimos saepe! Illo a quidem quis iusto explicabo eveniet ut veritatis est quo veniam quaerat atque temporibus, laborum minima consectetur ipsam adipisci mollitia dignissimos quod excepturi quia? Tempora quidem nobis, nulla et molestias ut, enim, quod incidunt debitis non nisi officia fugiat vero fuga temporibus iste expedita cupiditate nesciunt unde sit amet quibusdam saepe! Quisquam excepturi perspiciatis fugit dignissimos sit."
-        ],
-        [
-            "judul" => "My Superman 2",
-            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolorum dignissimos saepe! Illo a quidem quis iusto explicabo eveniet ut veritatis est quo veniam quaerat atque temporibus, laborum minima consectetur ipsam adipisci mollitia dignissimos quod excepturi quia? Tempora quidem nobis, nulla et molestias ut, enim, quod incidunt debitis non nisi officia fugiat vero fuga temporibus iste expedita cupiditate nesciunt unde sit amet quibusdam saepe! Quisquam excepturi perspiciatis fugit dignissimos sit."
-        ]
-    ];
-    return view('index', [
-        "title" => "home",
-        "posts" => $blog_posts
-    ]);
-});
+Route::get('/', [PostController::class, 'indexx']);
 
 
 Route::get('/about',function(){
     return view('about', [
-        "title" => "about",
+        "title" => "My Website",
         "name" => "Jeta Adhisatya",
         "email" => "jeta.adhisatya@binus.ac.id",
         "linkedin" => "https://www.linkedin.com/in/jeta-adhisatya/"
     ]);
 });
+
+Route::get('/{slug}', [PostController::class, 'show']);
